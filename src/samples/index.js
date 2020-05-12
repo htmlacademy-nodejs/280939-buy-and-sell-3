fb437021804f8ce25a0a1beecaff0b87ee6d51d5
topus009 <sj_89@mail.ru>
2020-05-12 20:08:41 +0300

    add sample files

---
+++ src/samples/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const type = require(`./type`);
+const title = require(`./title`);
+const description = require(`./description`);
+const category = require(`./category`);
+
+module.exports = {
+  type,
+  title,
+  description,
+  category,
+};
---
+++ src/samples/type.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = [
+  `offer`,
+  `sale`,
+];
