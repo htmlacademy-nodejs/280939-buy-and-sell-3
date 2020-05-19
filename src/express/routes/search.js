4ec6c262efe0262d06ec914a85674f7b9cc43e80
topus009 <sj_89@mail.ru>
2020-05-20 02:46:20 +0300

    fix search-results template

--- src/express/routes/search.js
+++ src/express/routes/search.js
@@ -2,8 +2,14 @@
 
 const {Router} = require(`express`);
 
+const newTicketsListData = require(`../../../templateData/newTicketsListData.json`);
+const searchResults = require(`../../../templateData/searchResults.json`);
+
 const SearchRouter = new Router();
 
-SearchRouter.get(`/`, (req, res) => res.render(`search/search-result`));
+SearchRouter.get(`/`, (req, res) => res.render(`search/search-result`, {
+  searchResults,
+  newTicketsListData
+}));
 
 module.exports = SearchRouter;
