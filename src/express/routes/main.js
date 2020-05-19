e6b016a88b4fb7c508ac675efdeccca165ffd64e
topus009 <sj_89@mail.ru>
2020-05-20 00:37:13 +0300

    add puglint & fixes & fixturesData

--- src/express/routes/main.js
+++ src/express/routes/main.js
@@ -2,9 +2,15 @@
 
 const {Router} = require(`express`);
 
+const newTicketsListData = require(`../../../templateData/newTicketsListData.json`);
+const popularTicketsListData = require(`../../../templateData/popularTicketsListData.json`);
+
 const MainRouter = new Router();
 
-MainRouter.get(`/`, (req, res) => res.render(`main/main`));
+MainRouter.get(`/`, (req, res) => res.render(`main/main`, {
+  newTicketsListData,
+  popularTicketsListData,
+}));
 MainRouter.get(`/register`, (req, res) => res.render(`main/sign-up`));
 MainRouter.get(`/login`, (req, res) => res.render(`main/login`));
 
--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -5,6 +5,6 @@ const {Router} = require(`express`);
 const MyRouter = new Router();
 
 MyRouter.get(`/`, (req, res) => res.render(`tickets/my-tickets`));
-MyRouter.get(`/comments`, (req, res) => res.send(`my/comments`));
+MyRouter.get(`/comments`, (req, res) => res.render(`tickets/comments`));
 
 module.exports = MyRouter;
