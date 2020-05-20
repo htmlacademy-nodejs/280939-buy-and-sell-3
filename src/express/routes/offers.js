76c5adb13bb358718afe2cfc655f656527b88adc
topus009 <sj_89@mail.ru>
2020-05-20 21:37:07 +0300

    fix [categories] templates & pagination &  fixes

--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -2,8 +2,18 @@
 
 const {Router} = require(`express`);
 
+const {
+  categoryTicketsListData,
+  categoriesListData,
+} = require(`../../../templateData/bd.js`);
+
+
 const OffersRouter = new Router();
 
+OffersRouter.get(`/category`, (req, res) => res.render(`category/category`, {
+  categoryTicketsListData,
+  categoriesListData,
+})); // возможно нет такого роута - добавил чтобы пристроить одноименный шаблон
 OffersRouter.get(`/category/:id`, (req, res) => res.send(`/offers/category/:id`));
 OffersRouter.get(`/add`, (req, res) => res.render(`tickets/new-ticket`));
 OffersRouter.get(`/edit/:id`, (req, res) => res.render(`tickets/ticket-edit`));
