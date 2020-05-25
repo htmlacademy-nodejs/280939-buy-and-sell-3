3fb0c1032d10fa2978cb9d3644539811d82dcf5a
topus009 <sj_89@mail.ru>
2020-05-25 21:04:56 +0300

    fix 2

--- src/service/cli/server/routes.js
+++ src/service/cli/server/routes.js
@@ -8,7 +8,7 @@ const {
   HTTP_NOT_FOUND_CODE,
 } = HTTP_CODES;
 
-const getDefautRoute = (res, message) => {
+const getDefaultRoute = (res, message) => {
   writeHead(res, HTTP_NOT_FOUND_CODE, `text/plain`);
   res.end(message || `Не повезло, :(`);
 };
@@ -16,16 +16,16 @@ const getDefautRoute = (res, message) => {
 const getBaseRoute = async (res) => {
   const filePath = `${process.cwd()}/mocks.json`;
   let data;
+  let jsonData;
   try {
     data = await readFileAsync(filePath, true);
+    jsonData = JSON.parse(data);
   } catch (err) {
     if (err) {
-      data = `[]`;
-      getDefautRoute(res);
+      getDefaultRoute(res);
     }
   }
 
-  const jsonData = JSON.parse(data);
   if (jsonData.length) {
     const template = [];
     template.push(`<ul>\n`);
@@ -36,11 +36,11 @@ const getBaseRoute = async (res) => {
     writeHead(res, HTTP_SUCCESS_CODE);
     res.end(template.join(``));
   } else {
-    getDefautRoute(res, `Not found`);
+    getDefaultRoute(res, `Not found`);
   }
 };
 
 module.exports = {
   '/': getBaseRoute,
-  "default": getDefautRoute,
+  "default": getDefaultRoute,
 };
